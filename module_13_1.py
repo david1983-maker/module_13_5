8717b67709430b414ad747b081847ea84867b147
david1983-maker <davidkilidzan94127@gmail.com>
2024-10-15 10:21:00 +0300

    Update module_13_5.py

--- module_13_1.py
+++ module_13_1.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = '7828289368:AAECdivr28cSv5QD0RIxxhlGun6fhul0sYY'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 kb = ReplyKeyboardMarkup(resize_keyboard=None)
